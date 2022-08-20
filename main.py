9c0b83493876c04e5786bc1cfeea7cc72c24a307
Amanda-Larson <larson.amandarae@gmail.com>
2022-08-19 21:34:30 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+import time as t
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_time():
+    time = int(t.time())
+    unix_time = f"In unix time, the time is now {time}."
+    return unix_time
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("Port", 6738))
+    app.run(host='0.0.0.0', port=port)
